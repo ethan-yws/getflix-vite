8c01f2925b7eb74453c34be287e182d47a7191ea
Ethan Shi <ethanshi0725@outlook.com>
2025-06-08 18:31:42 +1000

    Add Auth0 authentication to HeaderBar and wrap App in Auth0Provider

--- src/components/HeaderBar/HeaderBar.tsx
+++ src/components/HeaderBar/HeaderBar.tsx
@@ -1,12 +1,33 @@
-import { Link } from "react-router-dom";
-import { Logo, Wrapper } from "./HeaderBar.styles";
+import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Logo, Wrapper } from './HeaderBar.styles';
 
 export const HeaderBar: React.FC = () => {
-  return (
-    <Wrapper>
-      <Link to="/" style={{ textDecoration: "none" }}>
-        <Logo>GETFLIX</Logo>
-      </Link>
-    </Wrapper>
-  );
+    const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+
+    return (
+        <Wrapper>
+            <Link to="/" style={{ textDecoration: 'none' }}>
+                <Logo>GETFLIX</Logo>
+            </Link>
+            {isAuthenticated ? (
+                <>
+                    <span>Welcome, {user?.name}</span>
+                    <button
+                        onClick={() =>
+                            logout({
+                                logoutParams: {
+                                    returnTo: window.location.origin,
+                                },
+                            })
+                        }
+                    >
+                        Log Out
+                    </button>
+                </>
+            ) : (
+                <button onClick={() => loginWithRedirect()}>Log In</button>
+            )}
+        </Wrapper>
+    );
 };
