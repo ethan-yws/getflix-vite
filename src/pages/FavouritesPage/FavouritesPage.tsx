8b5468c8e784ec3292f2216cf156ae862789cc89
Ethan Shi <ethanshi0725@outlook.com>
2025-06-10 21:46:34 +1000

    refactor: remove unused imports from FavouritesPage component

--- src/pages/FavouritesPage/FavouritesPage.tsx
+++ src/pages/FavouritesPage/FavouritesPage.tsx
@@ -1,8 +1,3 @@
-import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-
 export const FavouritesPage = () => {
-  const { user } = useAuth0();
-
   return <div>FavouritesPage</div>;
 };
